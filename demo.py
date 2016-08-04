d2603605c11ac96e5435d18243ab47882cf481e9
Ko <jko1@LAMU02RJ49CG8WP.uhc.com>
2016-08-03 23:53:12 -0400

    created a library for the CARE system

---
+++ demo.py
@@ -0,0 +1,13 @@
+from CARE import CARE, Patient
+import time
+
+start_time = time.time()
+file1 = 'file1.csv'
+file2 = 'file2.csv'
+file3 = 'file3.csv'
+patient_zero = Patient('1', '0', '60', ['27509', '30000', 'V700'], '05/31/1994')
+
+careobj = CARE(file1)
+careobj.predict(patient_zero, 'ICARE')
+
+print('--- %s seconds ---' %(time.time() - start_time))
