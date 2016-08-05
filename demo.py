e8a177768e08049216188da90d69b682d5b15029
Ko <jko1@LAMU02RJ49CG8WP.uhc.com>
2016-08-05 16:50:25 -0400

    added documentation

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ file = '~/desktop/file1.csv'
 patient_zero = Patient('1', '0', '60', ['27509', '30000', 'V700'], '05/31/1994')
 
 careobj = CARE(file)
-#careobj.predict(patient_zero, 'CARE')
+careobj.predict(patient_zero, 'ICARE')
 #print(careobj.accuracy('ICARE'))
 
 print('--- %s seconds ---' %(time.time() - start_time))
